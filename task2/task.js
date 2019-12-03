1e0a92c0c60191711d49a018017b23f944f72084
Bohdan <balovbohdan@gmail.com>
2019-12-03 09:31:32 +0200

    reseted to the initial frame

--- task2/task.js
+++
@@ -1,6 +0,0 @@
-const utils = require('./utils');
-
-utils.flushOutputFile()
-  .then(utils.processInput)
-  .then(() => console.log('Output file created: /tmp/task2.txt'))
-  .catch(err => console.error(err));
--- task3.2/src/task.js
+++
@@ -1,6 +0,0 @@
-import * as utils from './utils';
-
-utils.flushOutputFile()
-  .then(utils.processInput)
-  .then(() => console.log('Output file created: /tmp/task2.txt'))
-  .catch(err => console.error(err));
