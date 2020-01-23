05d836c4d4b509464d2e1852f499ee5ab440d1ab
Bohdan <balovbohdan@gmail.com>
2020-01-23 08:45:21 +0200

    added config and aliases

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    model: {
+        postgresql: {
+            uri: 'postgres://postgres:1111@localhost:5432/postgres'
+        }
+    }
+};
--- src/model/data-access/sequelize.ts
+++ src/model/data-access/sequelize.ts
@@ -1,3 +1,5 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('postgres://postgres:1111@localhost:5432/postgres');
+import { config } from '#config';
+
+export const sequelize = new Sequelize(config.model.postgresql.uri);
