0945ea1dead150eeac8110943643fd4b60a1d638
Bohdan <balovbohdan@gmail.com>
2020-02-04 15:52:54 +0200

    del -> delete

--- src/model/actions/user-group/types.ts
+++ src/model/actions/user-group/types.ts
@@ -1,4 +1,4 @@
-export type DelUserGroupInput = {
+export type DeleteUserGroupInput = {
     where?: {
         id?: string;
         userId?: string;
--- src/model/actions/user-group/user-group.ts
+++ src/model/actions/user-group/user-group.ts
@@ -4,7 +4,7 @@ import { UserGroup } from '#model/data-access';
 
 import * as T from './types';
 
-export const del = async (input: T.DelUserGroupInput, transaction?: Transaction): Promise<void> => {
+export const del = async (input: T.DeleteUserGroupInput, transaction?: Transaction): Promise<void> => {
     const where = input.where as any;
 
     await UserGroup.destroy({
