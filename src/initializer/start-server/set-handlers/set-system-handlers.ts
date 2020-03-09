84ed49692997f0e90428521f3da9eaae56333bed
balovbohdan <balovbohdan@gmail.com>
2020-03-09 09:02:55 +0200

    cors middleware added

--- src/initializer/start-server/set-handlers/set-system-handlers.ts
+++ src/initializer/start-server/set-handlers/set-system-handlers.ts
@@ -1,6 +1,8 @@
+import * as cors from 'cors';
 import * as express from 'express';
 
 const setSystemHandlers = (app: express.Express) => {
+    app.use(cors());
     app.use(express.json());
 
     app.use((err, req, res, next) => {
