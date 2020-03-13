86b2fd2024734ce68b6d2a9b9fcd93f840f5d6bb
balovbohdan <balovbohdan@gmail.com>
2020-03-13 13:38:55 +0200

    added time logger

--- src/initializer/start-server/set-handlers/set-system-handlers.ts
+++ src/initializer/start-server/set-handlers/set-system-handlers.ts
@@ -1,6 +1,11 @@
-import * as express from 'express';
+import * as express from 'express'
+import * as responseTime from 'response-time';
 
 const setSystemHandlers = (app: express.Express) => {
+    app.use(responseTime((req, res, time) => {
+        console.log(`Req "${req.url}" took "${time}" milliseconds.`);
+    }));
+
     app.use(express.json());
 };
 
