3d3bee9af7dba5c3826314b5bb2bbe78e51ccd0b
balovbohdan <balovbohdan@gmail.com>
2020-04-20 10:06:07 +0300

    *.test.js -> *.test.ts

--- src/routers/middlewares/response-time-logger.ts
+++ src/routers/middlewares/response-time-logger.ts
@@ -1,15 +1,9 @@
-import * as responseTimeModule from 'response-time';
+const responseTime = require('response-time');
 
 import loggers from '#loggers';
-
-// Fix for "./response-time-logger.test.js".
-const responseTime = typeof responseTimeModule === 'function'
-    ? responseTimeModule
-    : responseTimeModule.default;
 
 const responseTimeLogger = responseTime((req, res, time) => {
     loggers.consoleLogger.info(`Req '${req.url}' took '${time}' milliseconds.`);
 });
 
 export default responseTimeLogger;
-
